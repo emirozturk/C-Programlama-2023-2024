877561ccf5cbfc137ac82f86cc8625f2d6131d5a
Emir Öztürk <emirozturk3@gmail.com>
2023-12-05 10:02:42 +0300

    Uygulayıcı eklendi

--- Uygulamalar/Uygulama8/Uygulama8/Program.cs
+++ Uygulamalar/Uygulama8/Uygulama8/Program.cs
@@ -1,15 +1,14 @@
 ﻿class Program
 {
-    public static int fonk(int a,int b)
+    public static int Uygulayici(int a,int b,Func<int,int,int> fonk)
     {
-        return a + b;
+        return fonk(a,b);
     }
     public static void Main(string[] args)
     {
         int a = 342;
         int b = 4398;
-        Console.WriteLine(fonk(a, b));
-        Func<int,int,int> fonk2 = (a, b) => a + b;
-        Console.WriteLine(fonk2(a,b));
+        Console.WriteLine(Uygulayici(a,b,(x,y)=>x+y));
+        Console.WriteLine(Uygulayici(a,b,(x,y)=>x*y));
     }
 }
