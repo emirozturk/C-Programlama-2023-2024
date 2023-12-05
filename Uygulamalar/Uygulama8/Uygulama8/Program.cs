e29fc6223b9d11a359fe2d3aaf6f2624990f9922
Emir Öztürk <emirozturk3@gmail.com>
2023-12-05 10:34:10 +0300

    Update Program.cs

--- Uygulamalar/Uygulama8/Uygulama8/Program.cs
+++ Uygulamalar/Uygulama8/Uygulama8/Program.cs
@@ -7,7 +7,11 @@
     public static void Main(string[] args)
     {
         var sayilar = new List<int>() {0,-1,60,8,-1,15,2,9,2,84,3,30,7,48,31,47,38,2,42,66,31,28,43,28,79,48,52,96,17,76,55,20,18,57,44,56,36,51,58,74,20,97,59,53,53,97,26,24,92,74,56,58,64,43,-1,62,94,48,47,23,-1,89,86,81,58,72,79,51,25,56,94,57,80,-1,7,31,59,34,100,-1,71};
-        var sonuc = sayilar.Any(x => x == -1);
-        Console.WriteLine(sonuc);
+        var temizlenmisSayilar = sayilar.Where(x => x != -1);
+        var sonuc = temizlenmisSayilar.All(x => x >= 0);
+        var ilkNotlar = temizlenmisSayilar.OrderBy(x => x).Take(2);
+        foreach(var eleman in ilkNotlar)Console.WriteLine(eleman);
+        ilkNotlar = temizlenmisSayilar.OrderByDescending(x => x).Take(2);
+        foreach(var eleman in ilkNotlar)Console.WriteLine(eleman);
     }
 }
