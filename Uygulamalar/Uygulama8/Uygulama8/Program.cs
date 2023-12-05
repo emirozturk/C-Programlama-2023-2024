26ee871168b9b940b5286ee0aa2297636646986e
Emir Öztürk <emirozturk3@gmail.com>
2023-12-05 09:58:51 +0300

    İlk fonksiyon tanımı

---
+++ Uygulamalar/Uygulama8/Uygulama8/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static int fonk(int a,int b)
+    {
+        return a + b;
+    }
+    public static void Main(string[] args)
+    {
+        int a = 342;
+        int b = 4398;
+        Console.WriteLine(fonk(a, b));
+        Func<int,int,int> fonk2 = (a, b) => a + b;
+        Console.WriteLine(fonk2(a,b));
+    }
+}
